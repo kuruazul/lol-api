42154a74ef76e9f31ccb7fcbd9790f42aac413e5
Erick calzada <ejcb200@hotmail.com>
2021-09-09 19:18:26 -0500

    nav desinging and some libriries

---
+++ lol-api/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Nav from './components/sections/Nav'
+function App() {  
+    return ( 
+    <div className = "App" >
+      <Nav></Nav>
+      </div>
+    );
+}
+
+export default App;
---
+++ lol-api/src/components/sections/Nav.jsx
@@ -0,0 +1,10 @@
+import '../../assets/css/Nav.css'
+const Nav = () => {
+    return(
+    <div className="text-center position-absolute w-100 title-box">
+    <span className="text-uppercase title-text fs-2">league of legends api</span>
+    </div>
+    )
+}
+
+export default Nav
