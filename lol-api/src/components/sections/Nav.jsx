dbe0757d27400a8ce2dccd8018ce8374ab1343d5
Erick calzada <ejcb200@hotmail.com>
2021-09-16 19:54:28 -0500

    adding translate

--- lol-api/src/components/sections/Nav.jsx
+++ lol-api/src/components/sections/Nav.jsx
@@ -1,14 +1,33 @@
 import '../../assets/css/Nav.css'
+
+import i18next from 'i18next'
+import { useState } from 'react'
+
 const Nav = () => {
+    const [lang,setLang] = useState(localStorage.getItem("lang") ? localStorage.getItem("lang"): "es")
+    const changeLang=(e)=>{
+        
+            if(i18next.language === "es"){
+                setLang(i18next.language)
+                i18next.changeLanguage("en")
+            }
+            else{
+                setLang(i18next.language)
+                i18next.changeLanguage("es")
+            }
+            let lng = i18next.language
+            localStorage.setItem("lang",lng)
+    }
     return(
     <div className="text-center position-absolute w-100 title-box">
-    <div className="position-absolute justify-content-end w-100 row">
-        <div className="btn-lang-box position-fixed  w-auto">
-            <button className="btn-lang w-auto border border-1 border-warning text-uppercase position-fixed"> en</button>
+        <div className="position-absolute justify-content-end w-100 row">
+            <div className="btn-lang-box position-fixed  w-auto">
+                <button className="btn-lang w-auto border border-1 border-warning text-uppercase position-fixed" onClick={changeLang}>{lang}</button>
+            </div>
         </div>
-    </div>
-    <span className="text-uppercase title-text fs-2">league of legends api</span>
-    
+        <div className="text-title-box">
+        <span className="text-uppercase title-text">league of legends api</span>
+        </div>
     </div>
     )
 }
